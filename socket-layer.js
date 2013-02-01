adef82847839c82d11cd00fe18e9cd19c0f2c7ff
American Yak <langbert@gmail.com>
2013-02-01 11:18:03 -0500

    Heroku "XHR long polling" instead of sockets.

--- socket-layer.js
+++ socket-layer.js
@@ -2,6 +2,15 @@ module.exports = function(app, server) {
 
 	var io = require('socket.io').listen(server);
 
+	// purportedly this works
+	// https://devcenter.heroku.com/articles/using-socket-io-with-node-js-on-heroku
+	// http://stackoverflow.com/questions/6223867/can-i-set-up-socket-io-chat-on-heroku
+	// http://robdodson.me/blog/2012/06/04/deploying-your-first-node-dot-js-and-socket-dot-io-app-to-heroku/
+	io.configure(function() {
+		io.set("transports", ["xhr-polling"]);
+		io.set("polling duration", 10);
+	});
+
 	io.sockets.on('connection', function(socket) {
 
 		console.log('Socket connection...');
