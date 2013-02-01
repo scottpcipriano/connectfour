9bcba6920bec21bc22466bc3451461975c42f379
Scott Cipriano <spcip82@gmail.com>
2013-02-01 03:52:50 -0500

    Working Google based authentication!

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express'),
 	exphbs  = require('express3-handlebars'),
+	passport = require('passport'),
+	util = require('util'),
+	GoogleStrategy = require('passport-google').Strategy,
 	socketLayer = require('./socket-layer'),
 	app = module.exports = express(),
 	hbs = exphbs.create({ /* config */ }),
@@ -8,7 +11,6 @@ var express = require('express'),
 	mongoUrl = config.get('MONGO_URL'),
 	mongoose = require('mongoose'),
 	model = require('./models/model')(app,mongoose),
-	mainRoutes = require('./routes/main')(app,model),
 	http = require('http'),
 	path = require('path'),
 	server;
@@ -21,6 +23,43 @@ mongoose.connect(mongoUrl, function(err) {
 	}
 });
 
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Google profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in passport require a `validate` function, which accept
+//   credentials (in this case, an OpenID identifier and profile), and invoke a
+//   callback with a user object.
+passport.use(new GoogleStrategy({
+    returnURL: 'http://localhost:3000/auth/google/return',
+    realm: 'http://localhost:3000/'
+  },
+  function(identifier, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // To keep the example simple, the user's Google profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Google account with a user record in your database,
+      // and return that user instead.
+      profile.identifier = identifier;
+      return done(null, profile);
+    });
+  }
+));
+
 app.configure(function() {
 	app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 	app.set('port', process.env.PORT || port);
@@ -29,12 +68,20 @@ app.configure(function() {
 	app.use(express.favicon());
 	// using short format instead of dev because it doesn't log colors
 	app.use(express.logger('short'));
+  app.use(express.cookieParser());
 	app.use(express.bodyParser());
-	app.use(express.methodOverride());
-	app.use(app.router);
+	// must be before passport.session()
+	app.use(express.session({ secret: 'SBO' }));
+	app.use(express.methodOverride());	
+  app.use(passport.initialize());
+  app.use(passport.session());	
 	app.use(require('stylus').middleware(__dirname + '/public'));
 	app.use(express.static(path.join(__dirname, 'public')));
+  app.use(app.router);
 });
+
+// routes have to be defined after passport is defined
+var	mainRoutes = require('./routes/main')(app,model,passport);
 
 app.configure('development', function() {
 	app.use(express.errorHandler());
--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,14 @@
-module.exports = function(app,model) {
+module.exports = function(app,model,passport) {
 
 	var users = require('../controllers/users');
 	var games = require('../controllers/games');
 
+	
+
 	app.get('/', function(req, res) {
 		games.list(function(games) {
 			res.render('index', {
+				user: req.user, 
 				className: 'home',
 				title: 'Connect Four',
 				games: games
@@ -13,9 +16,10 @@ module.exports = function(app,model) {
 		});
 	});
 
-	app.get('/game/:id'	, function(req,res) { 
+	app.get('/game/:id'	, ensureAuthenticated, function(req,res) { 
 		games.get(req.params.id, function(game) {
 			res.render('game', {
+				user: req.user,
 				className: 'game',
 				isGame: true,
 				game: game
@@ -28,6 +32,7 @@ module.exports = function(app,model) {
 		users.create(function() {
 			users.count(function(length) {
 				res.render('users', {
+					user: req.user,
 					className: 'users',
 					title: 'Users List Page',
 					userTotal: length
@@ -42,5 +47,33 @@ module.exports = function(app,model) {
 			className: 'eggz',
 			title: 'Easter Eggz, yay!'
 		});	});
+
+	app.get('/auth/google',
+	  passport.authenticate('google'),
+	  function(req, res){
+	    // The request will be redirected to Google for authentication, so
+	    // this function will not be called.
+	  });
+	
+	app.get('/auth/google/return', 
+	  passport.authenticate('google', { failureRedirect: '/login' }),
+	  function(req, res) {
+	    // Successful authentication, redirect home.
+	    res.redirect('/');
+	  });
+  
+	app.get('/logout', function(req, res){
+	  req.logout();
+	  res.redirect('/');
+	});
+
+
+	// verifies user is authenticated before visiting page
+	// function should probably be moved as it is middleware
+	function ensureAuthenticated(req, res, next) {
+	  if (req.isAuthenticated()) { return next(); }
+	  res.redirect('/login')
+	}
+
 
 };
