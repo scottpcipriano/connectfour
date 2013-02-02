6aea686c41dfd6d3811405d29a93c44991e82012
Scott Cipriano <spcip82@gmail.com>
2013-02-02 01:21:51 -0500

    Display on game page who's turn it is

--- controllers/games.js
+++ controllers/games.js
@@ -6,7 +6,7 @@ var mongoose = require('mongoose'),
 exports.create = function (user,callback) {
   var game = new Game();
   game.turn = 1;
-  game.player_1_id = user.email;
+  game.player_1_email = user.email;
   game.save(function (err) {
   	callback(game);
   });
@@ -29,14 +29,14 @@ exports.list = function (callback)  {
 
 // return list of games associated with a user
 exports.listForUser = function (user, callback)  {
-  Game.find({$or:[{player_1_id: user.email },{player_2_id: user.email }]}, function(err, games) {
+  Game.find({$or:[{player_1_email: user.email },{player_2_email: user.email }]}, function(err, games) {
       callback(games);
   });
 }
 
 // return list of games not associated with the user
 exports.listOtherGames = function (user, callback)  {
-  Game.find({$nor:[{player_1_id: user.email },{player_2_id: user.email }]}, function(err, games) {
+  Game.find({$nor:[{player_1_email: user.email },{player_2_email: user.email }]}, function(err, games) {
       callback(games);
   });
 }
